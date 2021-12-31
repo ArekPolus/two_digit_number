e3822a7fba52a1de41d5b26f70c5322343276b4e
ArekPolus <72617381+ArekPolus@users.noreply.github.com>
2021-12-31 15:28:21 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+new_number = int(two_digit_number[0]) + int(two_digit_number[1])
+####################################
+#Write your code below this line 👇
+
+print(new_number)
+
+
+
+
+
